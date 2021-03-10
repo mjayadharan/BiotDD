e0215b2bbc6ef0e3b4cee8dda293610a1f330826
M Jayadharan <manu.jayadharan@gmail.com>
2021-03-10 13:47:41 -0500

    small change in main file.

--- src/biot_dd.cc
+++ src/biot_dd.cc
@@ -28,21 +28,21 @@ int main (int argc, char *argv[])
         mesh_m2d[2] = {3,3};
         mesh_m2d[3] = {2,2};
         mesh_m2d[4] = {1,1};
-        double c0=1;
+        double c0=1.0;
         double alpha=1;
-        int num_cycle=2;
+        int num_cycle=5;
         int max_itr=1000;
         double tolerence = 1.e-12;
-        BiotParameters bparam (0.01,5,c0,alpha);
+        BiotParameters bparam (0.01,100,c0,alpha);
 
 //        //BiotDD without mortar
 //        MixedBiotProblemDD<2> drained_split(1, bparam,0,0,1);
-//        MixedBiotProblemDD<2> fixed_stress(1,bparam,0,0,2);
-        MixedBiotProblemDD<2> monolithic(1,bparam,0,0,0);
+        MixedBiotProblemDD<2> fixed_stress(1,bparam,0,0,2);
+//        MixedBiotProblemDD<2> monolithic(1,bparam,0,0,0);
 
 //        drained_split.run (num_cycle, mesh_m2d, tolerence, max_itr);
-//        fixed_stress.run(num_cycle, mesh_m2d, tolerence, max_itr);
-        monolithic.run (num_cycle, mesh_m2d, tolerence, max_itr);
+        fixed_stress.run(num_cycle, mesh_m2d, tolerence, max_itr);
+//        monolithic.run (num_cycle, mesh_m2d, tolerence, max_itr);
 
      //BiotDD with mortar
 //        MixedBiotProblemDD<2> lin_mortar(1,bparam,1,1,0);
